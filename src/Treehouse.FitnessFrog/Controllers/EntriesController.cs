6a9c875e89ed90bfb1bc484033eef7850c404fc7
zeitmeister <simon.sporrong@hotmail.se>
2019-04-16 22:05:15 +0200

    Added validation summary section

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -56,7 +56,7 @@ namespace Treehouse.FitnessFrog.Controllers
         [HttpPost]
         public ActionResult Add(Entry entry)
         {
-            // If there aren't any "Duration" field validation errors
+                       // If there aren't any "Duration" field validation errors
             // then make sure that the duration is greater than 0.
             if(ModelState.IsValidField("Duration") && entry.Duration <= 0)
             {
